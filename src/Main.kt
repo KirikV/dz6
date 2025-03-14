c203fabc6ff3190c5ae64c2019d1ce969aa3753c
KirikV <anoprienkok@gmail.com>
2025-03-14 22:34:22 +0300

    part1

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
+import kotlin.random.Random
+import kotlin.random.nextInt
 
+fun main() {
+    createList()
+    println()
 }
+
+fun createList() {
+    val n = readln().toIntOrNull()
+    while (true) {
+        if (n != null && n > 0) {
+            val numbers = List(n) { Random.nextInt(-5..5) }
+            println(numbers)
+            numbers.toMutableList()
+            val num2 = numbers.map {
+                if (it % 2 == 0) it * 10
+                else it
+            }
+            println(num2)
+            val sumnum = num2.sumOf { it }
+            println(sumnum)
+            println(num2.filter { it > 0 })
+            return
+        } else {
+            println("Ошибка, попробуй снова")
+            createList()
+            return
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
